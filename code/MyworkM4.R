eb493453f7b3affb7543640d6ced40b2de0ae9b0
isurujcu <isuruwmic@gmail.com>
2024-05-15 10:13:15 +1000

    Moved the script to code folder

--- code/MyworkM4.R
+++ code/MyworkM4.R
@@ -3,3 +3,12 @@ library(usethis)
 use_git_config(user.name = "isurujcu", user.email = "isuruwmic@gmail.com")
 gitcreds::gitcreds_set()
 usethis ::use_github()
+
+install.packages("tidyverse")
+library(tidyverse)
+
+mpg
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy))
+ggplot()
+ggplot(data = mpg)
